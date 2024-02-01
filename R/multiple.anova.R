67ef86291a392a5dd06a2db2a6535b1175e50812
migfejo <miguel.ferriz98@gmail.com>
2024-02-01 10:37:52 +0100

    Basic package

--- R/multiple.anova.R
+++ R/multiple.anova.R
@@ -1,13 +1,13 @@
 #' @title Multiple Analysis of Variance
 #' @description Perform ANOVA separately for each of the variables in a
-#' `data.frame`.
-#' @param data data.frame containing the values of the variables for each
-#' sample. Values of the variables selected in `vars` must be `numeric`.
+#' \code{data.frame}.
+#' @param data \code{data.frame} containing the values of the variables for each
+#' sample. Values of the variables selected in \code{vars} must be \code{numeric}.
 #' @param group.var Vector containing group identifiers. Its length must match
-#' with the number of `data` rows, and its elements must be in the same order.
-#' @param vars String vector containing the names of the variables
-#' (matching colnames(data)) to analyze. Values of the selected `vars` must be
-#' `numeric`.
+#' with the number of \code{data} rows, and its elements must be in the same order.
+#' @param vars Default = \code{colnames(data)}. String vector containing the names of the variables
+#' (matching \code{colnames(data)}) to analyze. Values of the selected \code{vars} must be
+#' \code{numeric}.
 #' @return Vector containing resulting p-values for each test for each variable.
 #' @export multiple.t.test
 #'
@@ -16,7 +16,7 @@ multiple.anova <- function(data, group.var, vars = colnames(data)){
   res <- c()
   for (var in vars) {
     res <- c(res,
-             summary(aov(as.formula(paste0("`", var, "` ~ Group")), data = df))[[1]][1, "Pr(>F)"]
+             summary(stats::aov(stats::as.formula(paste0("`", var, "` ~ Group")), data = df))[[1]][1, "Pr(>F)"]
     )
   }
   names(res) <- vars
--- R/multiple.t.test.R
+++ R/multiple.t.test.R
@@ -2,13 +2,13 @@
 #' @description Perform the t-test separately for each of the variables in a
 #' \code{data.frame}.
 #' @param data \code{data.frame} containing the values of the variables for each
-#' sample. Values of the variables selected in `vars` must be `numeric`.
+#' sample. Values of the variables selected in \code{vars} must be \code{numeric}.
 #' @param vars String vector containing the names of the variables
-#' (matching colnames(data)) to analyze. Values of the selected `vars` must be
-#' `numeric`.
-#' @param group1 String vector containing names (matching rownames(data)) of
+#' (matching \code{colnames(data)}) to analyze. Values of the selected \code{vars} must be
+#' \code{numeric}.
+#' @param group1 String vector containing names (matching \code{rownames(data)}) of
 #' the first defined group.
-#' @param group2 String vector containing names (matching rownames(data)) of
+#' @param group2 String vector containing names (matching \code{rownames(data)}) of
 #' the second defined group.
 #' @return Vector containing resulting p-values for each test for each variable.
 #' @export multiple.t.test
@@ -17,7 +17,7 @@ multiple.t.test <- function(data, vars = colnames(data), group1, group2) {
   res <- c()
   for (var in vars) {
     res <- c(res,
-             t.test(data[group1, var], data[group2, var])$p.value)
+             stats::t.test(data[group1, var], data[group2, var])$p.value)
   }
   names(res) <- vars
   res
