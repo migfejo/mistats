ce3eaa89ee541be1faf6f60afab70d4180a66735
migfejo <miguel.ferriz98@gmail.com>
2024-02-07 09:34:15 +0100

    presence.grouping update

--- R/presence.grouping.R
+++ R/presence.grouping.R
@@ -11,6 +11,9 @@
 #' @param vars
 #' Variables from \code{data} to be classified. All of them must be \code{numeric}.
 #'
+#' @param lower.limit
+#' Minimum number allowed of members in each group. Minimum for t test is 2.
+#'
 #' @return
 #' A nested list. Each element (variable) contains a character vector
 #' of the names of the samples from each group, presence and absence, respectively,
@@ -18,23 +21,24 @@
 #'
 #' @export presence.grouping
 #'
-presence.grouping <- function(data, vars = colnames(data)) {
+presence.grouping <- function(data, vars = colnames(data), lower.limit = 2) {
   po <- list()  # po --> presence object
-  po <- po[vars]
-  names(po) <- vars
-
   for (x in vars) {
-    po[[x]]$presence <- rownames(data[data[, x] != 0 &
-                                        !is.na(data[, x]), ])
-    po[[x]]$absence  <- rownames(data[data[, x] == 0 &
-                                        !is.na(data[, x]), ])
-    if (anyNA(data[, x])) {
-      po[[x]]$na <- rownames(data[is.na(data[, x]), ])
+    if (all(nrow(data[data[, x] != 0 & !is.na(data[, x]), ]) >= lower.limit,
+            nrow(data[data[, x] == 0 & !is.na(data[, x]), ]) >= lower.limit)) {
+      po[[x]]$presence <- rownames(data[data[, x] != 0 &
+                                          !is.na(data[, x]), ])
+      po[[x]]$absence  <- rownames(data[data[, x] == 0 &
+                                          !is.na(data[, x]), ])
+      if (anyNA(data[, x])) {
+        po[[x]]$na <- rownames(data[is.na(data[, x]), ])
+      }
     }
   }
 
   po
 }
+
 
 
 
